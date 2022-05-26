1c99fa6a0da75fbe0d272a13ec658c69797af323
anton <garkesha@yandex.ru>
2022-05-26 09:14:53 +0300

    1. Создайте еще один контроллер, замаппленный на /cars. 2. Создайте модель Car с тремя произвольными полями. 3. Создайте список из 5 машин. 4. Создайте сервис с методом, который будет возвращать указанное число машин из созданного списка. 5. Создайте страницу cars.html. Реализуйте создание таблицы с машинами из сервиса с помощью thymeleaf. 6. При запросе /cars выводить весь список. При запросе /cars?count=2 должен отобразиться список из 2 машин, при /cars?count=3 - из 3, и тд. При count ≥ 5 выводить весь список машин.

---
+++ src/main/java/web/dao/CarDaoImp.java
@@ -0,0 +1,29 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CarDaoImp implements CarDao {
+    private List<Car> cars;
+
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car("Mercedes", "E500", 240));
+        cars.add(new Car("BMW", "535", 230));
+        cars.add(new Car("Audi", "Q7", 200));
+        cars.add(new Car("Porsche", "911", 300));
+        cars.add(new Car("Ford", "Mondeo", 180));
+    }
+
+    @Override
+    public List<Car> countCar(int count) {
+        if (count > 5) return cars;
+        return cars.stream().limit(count).collect(Collectors.toList());
+    }
+}
